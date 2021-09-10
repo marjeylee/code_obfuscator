f9596a062369f5df1b354b81eff7d89fde8558ee
mar <marjey_lee@163.com>
2021-09-10 20:57:48 +0800

    11

--- exclude/exclude_word.py
+++ exclude/exclude_word.py
@@ -11,14 +11,18 @@ Change Activity:
 -------------------------------------------------
 """
 EXCLUDE_WORD_SET = {'coding', '__init__', '__main__', '__name__', 'APP', 'relu', 'str', 'int', '__file__',
-                    'path', 'forward', 'socket', 'lower', 'upper', 'and', 'dumps', 'started', 'with', 'open', 'now',
-                    'size', 'shape', 'item', 'data', 'line', 'class', 're', 'range', 'uint8', 'int16', 'int32',
+                    'path', 'forward', 'socket', 'count', 'norm', 'lower', 'upper', 'and', 'dumps', 'started', 'with',
+                    'open', 'cv2', 'pool', 'yaml', 'DataLoader', 'dataloader', 'cuda', 'Focus', 'SiLU', 'print', 'Conv',
+                    'now', 'C3', 'SPP', 'Concat', 'Detect', 'log', 'forward', 'aspp2', 'corner', 'canvas',
+                    'size', 'shape', 'item', 'data', 'line', 'aspp3', 'update', 'cpu', 'array',
+                    'class', 're', 'range',
+                    'uint8', 'int16', 'int32',
                     'time', 'run', 'files', 'uuid', 'float32', 'float64', 'join', 'realpath', 'copy', 'set', 'INFO',
                     'moves', 'xrange', 'dict', 'zip', 'set_verbosity', 'DEBUG', 'ERROR', 'move', 'imread', 'imwrite',
                     'makedirs', 'restore', 'keys', 'identity', 'setter', 'load', 'exp', 'concatenate', 'dot', 'extend',
                     'arcsin', 'sin', 'cos', 'shuffle', 'request', 'argv', 'close', 'exists', 'cluster', 'graph', 'i',
                     'N1', 'form', 'asarray', 'cast', 'type', 'resize', 'blur', 'filter2D', 'decay', 'normalize', 'size',
-                    'move', 'angle', 'self', 'flip','long'}
+                    'move', 'angle', 'self', 'flip', 'long'}
 
 NET_EXCLUDE = {'layer0', 'layer1', 'layer2', 'layer3', 'layer4', 'Session', 'slim', 'Counter', 'framework', 'scopes',
                'ResNet', 'SeparableConv2d', 'Block', 'conv1', 'bn1', 'bn2', 'DetectionNet', 'output', 'aspp1', 'out',
